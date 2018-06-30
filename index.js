9f05ef0483333d57833af6748ac045a555bbd295
= <=>
2018-06-30 11:49:47 +0700

    translate

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello world, I am a chat bot, i am using messenger bot')
 })
 app.listen(3000, () => console.log('Webhook server is listening, port 3000'))
