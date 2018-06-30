a3cddd25ee5366b7b9e86ebaafb36846c4bb62d7
= <=>
2018-06-30 22:00:21 +0700

    Can chat and translate with dialogflow

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -1,9 +1,10 @@
 const API_AI_TOKEN = '6dc628ac63d74e70a32beab1a1592b4f'
 const apiAiClient = require('apiai')(API_AI_TOKEN)
-const FACEBOOK_ACCESS_TOKEN = 'EAAF3LkdXbvIBALxIJqipi8rEzSokjssUWccYXPX67pmpSzzmHGZAXZCM0AwuRu0RSZAUhBE8mK9fW9PAZAi8LRntWaOPpKER7UDEQLUEX1HV1BBe5hdD4yp0upUHzlbhURDYM8XsWTf2DkP9JZBHAHluJAojxAPp9czp6hLZA4CQZDZD'
+const FACEBOOK_ACCESS_TOKEN = 'EAAF3LkdXbvIBAOTpLOZCuzMAPEpNZCBzO8BLk6bbPpmXhB9LHLiPvLPG6yMlkI6F43ZBb0B0ZAWokR4psgV0R3RyLhkyduojZAEHBhrBY0OH49viqDKVz1RgWjfo1IVmFoZAfBzMwrCRfhk35EYRsn3EqjebWLv0fwT3WiZA7Jj5AZDZD'
 const request = require('request')
+const translate = require('google-translate-api')
 const sendTextMessage = (senderId, text) => {
-			console.log(text)
+
 		 request({
 				 url: 'https://graph.facebook.com/v2.6/me/messages',
 				 qs: { access_token: FACEBOOK_ACCESS_TOKEN },
@@ -20,19 +21,43 @@ module.exports = (event) => {
 
 			const senderId = event.sender.id
 			const message = event.message.text
+			console.log(`message here is the original messages: ${message}`)
+ 
+			translate(message, {to: 'en'}).then(res => {
+			    
+			    const testMessage = res.from.text.value?res.from.text.value:res.text
 
-			const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'samrachgoogledialogflow'})
+			    const detectedLangauge = res.from.language.iso
+			    //=> I speak English
+			    console.log(`Here is the translate text: ${testMessage}`)
+			    console.log(`Here is the detected language: ${detectedLangauge}`);
+			    //=> nl
 
-			apiaiSession.on('response', (response) => {
+			    const apiaiSession = apiAiClient.textRequest(testMessage, {sessionId: 'samrachgoogledialogflow'})
 
-			 	const result = response.result.fulfillment.speech
-				console.log(result)
+				apiaiSession.on('response', (response) => {
 
-				sendTextMessage(senderId, result)
+			 		const result = response.result.fulfillment.speech
+
+ 					console.log(result)
+
+					translate(result, {to: detectedLangauge}).then(res => {
+
+					const returnToMessenger = res.text
+
+					console.log(`Text to facebook: ${returnToMessenger}`)
+
+					sendTextMessage(senderId, returnToMessenger)
+
+				}).catch(err => {
+				    console.error(err);
+				});
+				
 			 })
 
-			apiaiSession.on('error', error =>
-				console.log(error)
-			)
+			apiaiSession.on('error', error =>console.log(error))
 			 apiaiSession.end()
+			}).catch(err => {
+			    console.error(err);
+			});
 		}
