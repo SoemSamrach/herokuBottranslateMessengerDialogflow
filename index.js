3a972dc040fc2e913ae0dffde54bfcb2e7d03520
= <=>
2018-07-01 15:58:07 +0700

    test

--- index.js
+++ index.js
@@ -35,15 +35,23 @@ app.get('/', function (req, res) {
   res.send('Hello world, I am a chat bot, i am using messenger bot')
 })
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send('Error, wrong token')
-})
+const verificationController = require('./controllers/verification');
+
+const messageWebhookController = require('./controllers/messageWebhook');
+app.get('/webhook/', verificationController);
+app.post('/', messageWebhookController);
+
+// // for Facebook verification
+// app.get('/webhook/', function (req, res) {
+//   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+//     res.send(req.query['hub.challenge'])
+//   }
+//   res.send('Error, wrong token')
+// })
 
 // Spin up the server
 app.listen(app.get('port'), function() {
   console.log('running on port', app.get('port'))
 })
+
+
