8ef906c58f90efb1e442f47b34a92cd385b2e9a6
= <=>
2018-07-01 15:54:48 +0700

    test

--- index.js
+++ index.js
@@ -6,6 +6,13 @@
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
 
+// const verificationController = require('./controllers/verification');
+
+// const messageWebhookController = require('./controllers/messageWebhook');
+// app.get('/', verificationController);
+// app.post('/', messageWebhookController);
+
+
 'use strict'
 
 const express = require('express')
@@ -28,8 +35,15 @@ app.get('/', function (req, res) {
   res.send('Hello world, I am a chat bot, i am using messenger bot')
 })
 
-const verificationController = require('./controllers/verification');
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
+})
 
-const messageWebhookController = require('./controllers/messageWebhook');
-app.get('/Webhook/', verificationController);
-app.post('/', messageWebhookController);
+// Spin up the server
+app.listen(app.get('port'), function() {
+  console.log('running on port', app.get('port'))
+})
