7e6699429ac0e6b9f801f3e2efa36ca187a0653f
= <=>
2018-07-01 16:06:45 +0700

    test again

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -1,6 +1,6 @@
-const API_AI_TOKEN = '6dc628ac63d74e70a32beab1a1592b4f'//this is the api from dialogflow
+const API_AI_TOKEN = 'dbe138a32bc641938e62d93a21900d26'//this is the api from dialogflow
 const apiAiClient = require('apiai')(API_AI_TOKEN)
-const FACEBOOK_ACCESS_TOKEN = 'EAAF3LkdXbvIBACGT49DmZB8Ai1M1nU7uTqcoI2TjYmod0jRjTwDbER3YPz8ThAECCgYz5XtUo2lXU2Dft8x5ZC9R6Sk0yFZAXXUS8UBIcuJEaZAZCOtqS478u5QZCV82TBKrZBuISXkQmPqqXI48ov1ZCmvOn4DF41MLIuq1BsTgwgZDZD'
+const FACEBOOK_ACCESS_TOKEN = 'EAAPQXLQ7RAUBAJwf4SlKBaZBu339RkwHk654dBcTj6DpC4JYEXYCojHkHwq9NoaRkC8PzTBRqBDIEbo4nzGZChA5m6MfHv0Q1MEiOGF3FwT3oQwQDfYpfXK66V9dHlZAdPjNTqmsiBDPXClRhJRRGSjppGjajWVAw7vRnBJvwZDZD'
 // this is the facebook access token which is generated from Facebook page
 const request = require('request')// This is the request client library which request to Facebook
 
